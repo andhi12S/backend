3c14b62bef151272a5cbcb8272befea61c503bc5
andhi12S <138228705+andhi12S@users.noreply.github.com>
2023-07-26 11:05:19 +0700

    error refresh token backend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import db from "./config/Database.js";
 import userRoute from "./routes/UserRoute.js";
+dotenv.config();
 const app = express();
-
 try {
     await db.authenticate();
     await db.sync()
@@ -10,7 +13,8 @@ try {
 } catch (error) {
     console.error(error);
 }
-
+app.use(cors({ credentials:true, origin:'http://localhost:3000'}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(userRoute);
 
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createUser, getUsers, deleteUser, updateUser } from "../controllers/Users.js";
+import {  getUsers, Register, Login, Logout, createUser, deleteUser, updateUser} from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 const userRoute = express.Router();
 
-userRoute.get('/users', getUsers);
+userRoute.get('/users', verifyToken, getUsers);
+userRoute.post('/users', Register);
+userRoute.post('/login', Login);
+userRoute.delete('/logout', Logout);
+userRoute.get('/token', refreshToken);
 userRoute.delete('/users/:id', deleteUser);
 userRoute.patch('/users/:id', updateUser);
 userRoute.post('/users', createUser);
