ac089e862493c83a3bc76f08c3b6b0bf9cb95162
andhi12S <138228705+andhi12S@users.noreply.github.com>
2023-07-26 13:52:41 +0700

    troblue shooting

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -14,7 +14,7 @@ export const refreshToken = async(req, res)=>{
 if(!user[0]) return res.sendStatus(403);
  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded)=>{
     if(err) return res.sendStatus(403);
-    const userId = user[0].id;
+    const userId = user[0].id; 
     const name   = user[0].name;
     const email  = user[0].email; 
     const accessToken = jwt.sign({userId, name , email}, process.env.ACCES_TOKEN_SECRET,{
